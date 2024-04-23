5ffb9e71b08eeec3348924c75c71186662f9ca4e
danielpappa <daniel.pappalardo@icloud.com>
2024-04-23 14:27:02 +0200

    Change examples

--- frontend/app.py
+++ frontend/app.py
@@ -3,4 +3,4 @@ import backend.TextGenerator as rag
 
 ragger = rag.TextGenerator("google/gemma-2b-it", "danielpappa/philly_restaurants")
 
-gr.ChatInterface(ragger.generate_response, title = "🌭🍗 philly's food places 🍣🍔", examples = ["Where could I get some good Mexican food in philadelphia?", "What is the best pizza in town?", "Give me some healthy dining options nearby."], theme="Jameswiller/Globe", submit_btn = None, stop_btn = None, retry_btn = None, undo_btn = None, clear_btn = None).launch()
+gr.ChatInterface(ragger.generate_response, title = "🌭🍗 philly's food places 🍣🍔", examples = ["Which restaurant has large food portions in philadelphia?", "What is the best pizza in town?", "Give me some healthy dining options nearby."], theme="Jameswiller/Globe", submit_btn = None, stop_btn = None, retry_btn = None, undo_btn = None, clear_btn = None).launch()
